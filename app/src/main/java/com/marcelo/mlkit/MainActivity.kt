034ca6147d6cf7a5e5b11c17adaeb92fcd0fdd7e
Marcelo Luiz Pinheiro <marcelo.pinheiro@alunos.sc.senac.br>
2025-04-09 11:44:55 -0300

    add app icon

--- app/src/main/java/com/marcelo/mlkit/MainActivity.kt
+++ app/src/main/java/com/marcelo/mlkit/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.marcelo.mlkit
 
+import android.app.Activity.RESULT_OK
+import android.content.ContentResolver
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -40,77 +43,92 @@ import java.io.FileOutputStream
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Instance of GMS Document Scanner
-        val options = GmsDocumentScannerOptions.Builder()
-            .setScannerMode(SCANNER_MODE_FULL)
-            .setGalleryImportAllowed(true)
-            .setPageLimit(3)
-            .setResultFormats(RESULT_FORMAT_JPEG, RESULT_FORMAT_PDF)
-            .build()
-        val scanner = GmsDocumentScanning.getClient(options)
-
 
         enableEdgeToEdge()
         setContent {
             MLKitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Modifier.padding(innerPadding)
-                    var imageUris by remember {
-                        mutableStateOf<List<Uri>>(listOf())
-                    }
-                    val scannerLauncher = rememberLauncherForActivityResult(
-                        contract = ActivityResultContracts.StartIntentSenderForResult(),
-                        onResult = {
-                            if(it.resultCode == RESULT_OK) {
-                                val result = GmsDocumentScanningResult.fromActivityResultIntent(it.data)
-                                imageUris = result?.pages?.map { page -> page.imageUri }.orEmpty()
-
-                                result?.pdf?.let { pdf ->
-                                   val fos  = FileOutputStream(File(filesDir, "scan.pdf"))
-                                    contentResolver.openInputStream(pdf.uri)?.use { inputStream ->
-                                        inputStream.copyTo(fos)
-                                    }
-                                }
-                            }
-                        }
+                    ScannerScreen(
+                        filesDir = filesDir,
+                        contentResolver = contentResolver,
+                        activity = this
                     )
+                }
+            }
+        }
+    }
+}
 
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        imageUris.forEach { uri ->
-                            AsyncImage(
-                                model = uri,
-                                contentDescription = null,
-                                contentScale = ContentScale.FillWidth,
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                        }
-                        Button(
-                            onClick = {
-                                scanner.getStartScanIntent(this@MainActivity)
-                                    .addOnSuccessListener { intent ->
-                                        scannerLauncher.launch(
-                                            IntentSenderRequest.Builder(intent).build()
-                                        )
-                                    }
-                                    .addOnFailureListener {
-                                        Toast.makeText(
-                                            this@MainActivity,
-                                            "Error ${it.message}",
-                                            Toast.LENGTH_LONG
-                                        ).show()
-                                    }
-                            }
-                        ) {
-                            Text(text = "Scan Document")
-                        }
+@Composable
+private fun ScannerScreen(
+    filesDir: File,
+    contentResolver: ContentResolver,
+    activity: ComponentActivity
+) {
+
+    //Instance of GMS Document Scanner
+    val options = GmsDocumentScannerOptions.Builder()
+        .setGalleryImportAllowed(false)
+        .setPageLimit(2)
+        .setResultFormats(RESULT_FORMAT_JPEG, RESULT_FORMAT_PDF)
+        .setScannerMode(SCANNER_MODE_FULL)
+        .build()
+    val scanner = GmsDocumentScanning.getClient(options)
+
+    var imageUris by remember {
+        mutableStateOf<List<Uri>>(listOf())
+    }
+    val scannerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartIntentSenderForResult(),
+        onResult = {
+            if (it.resultCode == RESULT_OK) {
+                val result = GmsDocumentScanningResult.fromActivityResultIntent(it.data)
+                imageUris = result?.pages?.map { page -> page.imageUri }.orEmpty()
+
+                result?.pdf?.let { pdf ->
+                    val fos = FileOutputStream(File(filesDir, "scan.pdf"))
+                    contentResolver.openInputStream(pdf.uri)?.use { inputStream ->
+                        inputStream.copyTo(fos)
                     }
                 }
             }
+        }
+    )
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        imageUris.forEach { uri ->
+            AsyncImage(
+                model = uri,
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Button(
+            onClick = {
+                scanner.getStartScanIntent(activity)
+                    .addOnSuccessListener { intent ->
+                        scannerLauncher.launch(
+                            IntentSenderRequest.Builder(intent).build()
+                        )
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(
+                            activity,
+                            "Error ${it.message}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+            }
+        ) {
+            Text(text = "Scan Document")
         }
     }
 }
