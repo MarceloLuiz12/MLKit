2a72bdae964778960e5b22a47825f75ba467ad2e
Marcelo Luiz Pinheiro <marcelo.pinheiro@alunos.sc.senac.br>
2025-04-09 10:20:12 -0300

    Initial commit

---
+++ app/src/main/java/com/marcelo/mlkit/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.marcelo.mlkit
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
+import com.marcelo.mlkit.ui.theme.MLKitTheme
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
+import java.io.File
+import java.io.FileOutputStream
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Instance of GMS Document Scanner
+        val options = GmsDocumentScannerOptions.Builder()
+            .setScannerMode(SCANNER_MODE_FULL)
+            .setGalleryImportAllowed(true)
+            .setPageLimit(3)
+            .setResultFormats(RESULT_FORMAT_JPEG, RESULT_FORMAT_PDF)
+            .build()
+        val scanner = GmsDocumentScanning.getClient(options)
+
+
+        enableEdgeToEdge()
+        setContent {
+            MLKitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Modifier.padding(innerPadding)
+                    var imageUris by remember {
+                        mutableStateOf<List<Uri>>(listOf())
+                    }
+                    val scannerLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.StartIntentSenderForResult(),
+                        onResult = {
+                            if(it.resultCode == RESULT_OK) {
+                                val result = GmsDocumentScanningResult.fromActivityResultIntent(it.data)
+                                imageUris = result?.pages?.map { page -> page.imageUri }.orEmpty()
+
+                                result?.pdf?.let { pdf ->
+                                   val fos  = FileOutputStream(File(filesDir, "scan.pdf"))
+                                    contentResolver.openInputStream(pdf.uri)?.use { inputStream ->
+                                        inputStream.copyTo(fos)
+                                    }
+                                }
+                            }
+                        }
+                    )
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        imageUris.forEach { uri ->
+                            AsyncImage(
+                                model = uri,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                scanner.getStartScanIntent(this@MainActivity)
+                                    .addOnSuccessListener { intent ->
+                                        scannerLauncher.launch(
+                                            IntentSenderRequest.Builder(intent).build()
+                                        )
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Error ${it.message}",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                            }
+                        ) {
+                            Text(text = "Scan Document")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
